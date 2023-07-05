c89777552f208113f9fff9cf40757227f917335b
Sivalingam S <sivalingam.s@technoforte.co.in>
2023-07-05 13:28:42 +0530

    Switched to multiply

--- sub.java
+++ sub.java
@@ -1,8 +1,8 @@
 public class sub {
     public static void main(String []args)
     {
-        int a=70,b=10,c;
-        c=a-b;
+        int a=7,b=10,c;
+        c=a*b;
         System.out.println(c);
     }
 }
